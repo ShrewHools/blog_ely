eafaffded19a255d8ebcb340be2f0d7052165f6f
shrewhools <vladyukha@yandex.ru>
2016-08-01 20:35:38 +0300

    add MVC POST

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Post create"
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      flash[:success] = "Post update"
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = "Post DELETE"
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :post_body)
+    end
+
+
+end
