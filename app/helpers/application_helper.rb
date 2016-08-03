5e104aaa5aa14708494abb496dc7ededd79ce356
shrewhools <vladyukha@yandex.ru>
2016-08-03 20:46:40 +0300

    Style Header,footer, social ico

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,12 +8,12 @@ private
 
   def header_block_1
     @categories = Category.all
-    @categories_block_1 = @categories.limit(1)
+    @categories_block_1 = @categories.limit(2)
   end
 
   def header_block_2
     @categories = Category.all
-    @categories_block_2 = @categories.order("id DESC").limit(1)
+    @categories_block_2 = @categories.order("id DESC").limit(2)
   end
 
 end
